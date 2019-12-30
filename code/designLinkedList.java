1e06c57b776f29c7568ca8873c3e7d725c66f976
Hilal Tarakci <hlltarakci@gmail.com>
2019-12-30 14:38:15 +0300

    add solution for Design Linked List

---
+++ code/designLinkedList.java
@@ -0,0 +1,112 @@
+// https://leetcode.com/problems/design-linked-list/
+class MyLinkedList {
+    class Node {
+        public int val;
+        public Node next;
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+    
+    private Node head;
+
+    /** Initialize your data structure here. */
+    public MyLinkedList() {
+        head = new Node(-1);
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+        Node current = this.head.next;
+        while(index-- > 0) {
+            if(current == null) return -1;
+            current = current.next;
+        }
+        return current == null ? -1 : current.val;
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+        Node newNode = new Node(val);
+        newNode.next = this.head.next;
+        this.head.next = newNode;
+        this.debug("addAtHead");
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+        if(this.head.next == null) {
+            addAtHead(val);
+            return;
+        }
+        
+        Node newNode = new Node(val);
+        
+        Node current = this.head.next;
+        while(current.next != null) current = current.next;
+        
+        if(current != null) current.next = newNode;
+        this.debug("addAtTail");
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+        if(index == 0) {
+            addAtHead(val);
+            return;
+        }
+        
+        Node newNode = new Node(val);
+        
+        Node current = this.head.next;
+        while(--index > 0) {
+            if(current == null) return;
+            current = current.next;
+        }
+        
+        if(current != null) {
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        
+        this.debug("addAtIndex");
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+        if(index == 0) {
+            this.head.next = this.head.next.next;
+            return;
+        }
+        
+        Node current = this.head.next;
+        while(--index > 0) {
+            if(current == null) return;
+            current = current.next;
+        }
+        
+        if(current != null && current.next != null) current.next = current.next.next;
+        this.debug("deleteAtIndex");
+    }
+    
+    private void debug(String method) {
+        StringBuilder list = new StringBuilder();
+        list.append(method).append(": ");
+        Node current = this.head.next;
+        while(current != null) {
+            list.append(current.val).append(" -> ");
+            current = current.next;
+        }
+        // System.out.println(list.toString());
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
