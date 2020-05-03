6efe1d8ef3e0e6a4d485d9b5cf293807f3d229f6
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-03 22:23:25 +0300

    Update mergeTwoSortedLists.java

--- code/mergeTwoSortedLists.java
+++ code/mergeTwoSortedLists.java
@@ -1,48 +1,39 @@
 // https://leetcode.com/problems/merge-two-sorted-lists/
+// https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1296/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-	/*
-		clarifying questions & edge cases:
-		test: 
-		
-		algorithm:
-            iterate through both, add smaller one to result and go forward
-		
-        a: l1 node count
-        b: l2 node count
-		time complexity: O(a+b) 
-		space complexity: O(1) 
-	*/
+    /*
+        n, m: num of nodes
+        time: O(n+m)
+        space: O(1) 
+    */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
         
-        ListNode current = dummy;
-        while(l1 != null || l2 != null) {
-            if(l1 != null && l2 != null) {
-                if(l1.val < l2.val) {
-                    current.next = l1;
-                    l1 = l1.next;
-                } else {
-                    current.next = l2;
-                    l2 = l2.next;
-                }
-            } else if (l1 != null) {
-                current.next = l1;
+        ListNode curr = dummy;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                curr.next = l1;
                 l1 = l1.next;
-            } else if (l2 != null) {
-                current.next = l2;
+            } else {
+                curr.next = l2;
                 l2 = l2.next;
             }
-            
-            current = current.next;
+            curr = curr.next;
         }
+        
+        if(l1 != null) curr.next = l1;
+        if(l2 != null) curr.next = l2;
         
         return dummy.next;
     }
