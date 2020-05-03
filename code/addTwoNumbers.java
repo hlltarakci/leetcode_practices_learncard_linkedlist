913f25bf4fbe72af5bec538b45a5fb369bc4dcc9
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-03 22:24:19 +0300

    Update addTwoNumbers.java

--- code/addTwoNumbers.java
+++ code/addTwoNumbers.java
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/add-two-numbers/
+// https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1296/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,38 +11,30 @@
  */
 class Solution {
     /*
-		clarifying questions & edge cases:
-		test: 
-		
-		algorithm:
-            loop through, add
-		
-        a: node count of l1
-        b: node count of l2
-		time complexity: O(max(a,b))
-		space complexity: O(1) -- excluding result
-            result will use O(max(a,b)) space
-	*/
+        n, m: num of nodes
+        time: O( max(n, m))
+        space: O( max(n, m)) --> for output
+    */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(-1);
+        if(l1 == null || l2 == null) return null;
         
-        ListNode current = result;
-        int carry = 0, base = 10;
-        while(l1 != null || l2 != null || carry > 0) {
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
             int val1 = l1 != null ? l1.val : 0;
             int val2 = l2 != null ? l2.val : 0;
             
+            int val = val1 + val2 + carry;
+            curr.next = new ListNode(val%10);
+            carry = val/10;
+            
+            curr = curr.next;
             if(l1 != null) l1 = l1.next;
             if(l2 != null) l2 = l2.next;
-            
-            int val = (val1 + val2 + carry) % base;
-            carry = (val1 + val2 + carry) / base;
-            
-            ListNode element = new ListNode(val);
-            current.next = element;
-            current = current.next;
         }
         
-        return result.next;
+        if(carry > 0) curr.next = new ListNode(carry);
+        return dummy.next;
     }
 }
