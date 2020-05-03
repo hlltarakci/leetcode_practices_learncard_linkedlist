bc7eb8b10d315269c1def955bbc786e00565ccc3
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-03 22:19:08 +0300

    Update removeNthNodeFromEndOfList.java

--- code/removeNthNodeFromEndOfList.java
+++ code/removeNthNodeFromEndOfList.java
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+// https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1296/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,34 +11,27 @@
  */
 class Solution {
     /*
-		clarifying questions & edge cases:
-            list size n -- remove head
-            n is zero -- remove last node
-		test: 
-		    1->2->3->4->5, and n = 2
-            
-		algorithm: two pointer technique
-            fast pointer moves n+1 nodes ahead
-            slow pointer starts when fast pointer is n+1 nodes ahead
-            when fast pointer is done with list, delete what slow pointer points to
-            
-		n: node count
-		time complexity: O(n) -- one pass
-		space complexity: O(1)
-	*/
-	
+        n: num of nodes
+        time: O(n)
+        space: O(1)
+    */
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head, slow = head;
+        if(head == null || n <= 0) return null;
+        
+        ListNode fast = head;
         while(n-- > 0) fast = fast.next;
         
-        while(fast != null && fast.next != null) {
-            fast = fast.next;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode slow = dummy;
+        while(fast != null) {
             slow = slow.next;
+            fast = fast.next;
         }
         
-        if(fast == null) head = head.next;
-        else if(slow.next != null) slow.next = slow.next.next;
+        slow.next = slow.next.next;
         
-        return head;
+        return dummy.next;
     }
 }
