c6458ad0c6304b4bb751b41bdd33ec5ac58b1c3a
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-03 22:17:44 +0300

    Update linkedListCycleII.java

--- code/linkedListCycleII.java
+++ code/linkedListCycleII.java
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/linked-list-cycle-ii/
+// https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -11,52 +13,33 @@
  * }
  */
 public class Solution {
-	/*
-		clarifying questions & edge cases:
-		test: 
-		
-		algorithm: two pointer technique
-            fast pointer: double steps
-            slow pointer: single step
-            once they meet, this means there is cycle
-            at that point, take one of the pointers to head, move them one step at a time,
-                they'll meet at cycle start point
-		
-        n: node count
-		time complexity: O(n)
-		space complexity: O(1)
-	*/
-	
+    /*
+        n: num of nodes
+        time: O(n)
+        space: O(1)
+    */
     public ListNode detectCycle(ListNode head) {
-        ListNode meetPoint = getMeetPoint(head);
+        if(head == null) return null;
         
-        if(meetPoint != null) {
-            ListNode current = head;
-            while( meetPoint != current ) {
-                meetPoint = meetPoint.next;
-                current = current.next;
-            } 
-            
-            return current;
-        }
-        
-        return null;
-    }
-    
-    private ListNode getMeetPoint(ListNode head) {
-        ListNode fast = head, slow = head;
-        
-        boolean isCycle = false;
+        // detect cycle
+        ListNode slow = head, fast = head;
         while(fast != null && fast.next != null) {
-            fast = fast.next.next;
             slow = slow.next;
+            fast = fast.next.next;
             
-            if(fast == slow) {
-                isCycle = true;
-                break;
-            } 
+            if(slow == fast) break;
         }
         
-        return isCycle ? fast : null;
+        if(fast == null || fast.next == null) return null;
+        
+        slow = head;
+        
+        while(true) {
+            if(slow == fast) break;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
     }
 }
